fec61d3cecfc5a8609a0939e8ad569c96d227496
SvetlanaBuuvol <divinesvetok@gmail.com>
2023-04-26 23:58:19 +0300

    task-2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,19 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsEl = document.querySelector('#ingredients');
+
+const createIngredients = ingredients.map(ingredient => {
+
+  const listEl = document.createElement('li');
+  listEl.classList.add('item');
+  listEl.textContent = ingredient;
+
+  return listEl;
+});
+
+ingredientsEl.append(...createIngredients);
+
+
+
