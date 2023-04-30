7163787a0431dad85056b43dedb068a9431afafb
SvetlanaBuuvol <divinesvetok@gmail.com>
2023-04-30 22:44:18 +0300

    task-9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
+const body = document.body;
+const button = document.querySelector('.change-color');
+const color = document.querySelector('.color');
+
+button.addEventListener('click', onButtonClick);
+
+function onButtonClick(event) {
+  const randomColor = getRandomHexColor();
+  body.style.backgroundColor = randomColor;
+  color.textContent = randomColor;
+};
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
