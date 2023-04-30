ed9d63353aca43b69f8af9d262a396786112ec8d
SvetlanaBuuvol <divinesvetok@gmail.com>
2023-04-30 22:26:51 +0300

    task-8

--- js/task-08.js
+++ js/task-08.js
@@ -1,2 +1,23 @@
 const form = document.querySelector('.login-form');
+const input = document.querySelectorAll('input');
 
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+
+    const mail = event.currentTarget.elements.email.value;
+    const password = event.currentTarget.elements.password.value;
+ 
+    if (mail.length === 0 || password.length === 0) {
+        alert('Warning! All fields must be filled!');
+
+    } else {
+        const formData = {
+        mail,
+        password,
+    };
+        console.log(formData);
+        form.reset();
+    }
+};
